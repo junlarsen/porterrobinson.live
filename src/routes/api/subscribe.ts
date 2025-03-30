e2c5cfd0d1912ac22aaca5b2b70b09f89f81c82a
Mats Jun Larsen <mats@jun.codes>
2025-03-30 12:22:39 +0200

    fix: give stable ids to calendar events

--- src/routes/api/subscribe.ts
+++ src/routes/api/subscribe.ts
@@ -1,3 +1,4 @@
+import * as crypto from "node:crypto";
 import { createAPIFileRoute } from "@tanstack/react-start/api";
 import ical from "ical-generator";
 import { getEventZonedTime, getEvents } from "~/events";
@@ -12,7 +13,13 @@ export const APIRoute = createAPIFileRoute("/api/subscribe")({
       prodId: "porterrobinson.live",
     });
     for (const event of events) {
+      const id = crypto
+        .createHash("sha256")
+        .update(event.name)
+        .update(event.link)
+        .digest("hex");
       calendar.createEvent({
+        id,
         start: getEventZonedTime(event),
         summary: event.name,
         description: `Porter Robinson is live playing ${event.name}`,
